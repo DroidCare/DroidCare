8cbe08d9b0c417ff9ae9ac113aaa69d3abcf2c79
edocsss <edocsss@gmail.com>
2015-03-08 10:35:56 +0800

    Added JavaDoc for ConsultantAppointmentManager.

--- src/com/droidcare/ConsultantAppointmentManager.java
+++ src/com/droidcare/ConsultantAppointmentManager.java
@@ -3,15 +3,27 @@ package com.droidcare;
 import android.content.Context;
 import android.widget.Toast;
 
+/**
+ * 
+ * @author Edwin Candinegara
+ * A subclass of {@link AppointmentManager}. Instantiated only when the user is a consultant. This class
+ * is responsible for accepting and rejecting pending appointment.
+ *
+ */
+
 public class ConsultantAppointmentManager extends AppointmentManager {
+	/**
+	 * Empty constructor. Only call super().
+	 */
 	public ConsultantAppointmentManager () {
 		super();
 	}
 	
-	/*
-	 * Add Consultant specific methods here
+	/**
+	 * Accepts a pending {@link Appointment} object.
+	 * @param context		the context from which this method is called.
+	 * @param appointment	a pending {@link Appointment} object to be accepted by the consultant.
 	 */
-	
 	public void acceptAppointment (Context context, Appointment appointment) {
 		if (appointment.getStatus() == Appointment.PENDING) {
 			appointment.setStatus(Appointment.ACCEPTED);
@@ -27,6 +39,11 @@ public class ConsultantAppointmentManager extends AppointmentManager {
 		}
 	}
 	
+	/**
+	 * Rejects a pending {@link Appointment} object.
+	 * @param context		the context from which this method is called.
+	 * @param appointment	a pending {@link Appointment} object to be rejected by the consultant.
+	 */
 	public void rejectAppointment (Context context, Appointment appointment) {
 		if (appointment.getStatus() == Appointment.PENDING) {
 			appointment.setStatus(Appointment.REJECTED);
