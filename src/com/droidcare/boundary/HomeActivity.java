3cce8b0f3c01d75a844de85eef6bc9b692a94a6f
Peter <peterch1ang@yahoo.com>
2015-03-11 14:57:06 +0800

    Reset state in HomeActivity

--- src/com/droidcare/boundary/HomeActivity.java
+++ src/com/droidcare/boundary/HomeActivity.java
@@ -35,6 +35,11 @@ public class HomeActivity extends FragmentActivity implements ActionBar.TabListe
 		setContentView(R.layout.activity_home);
 
         Global.initAppointmentManager();
+        AppointmentManager appointmentManager = Global.getAppointmentManager();
+        appointmentManager.clearRejectedAppointments();
+        appointmentManager.clearAcceptedAppointments();
+        appointmentManager.clearFinishedAppointments();
+        appointmentManager.clearPendingAppointments();
 
         Log.d("DEBUGGING", "HomeActivity onCreate");
         Global.getAppointmentManager().retrieveAppointmentList(new AppointmentManager.OnFinishListener() {
