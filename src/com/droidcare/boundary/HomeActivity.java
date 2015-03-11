c8d6f58c180dafea6bde7ca9b18bdd6905c46f1e
Peter <peterch1ang@yahoo.com>
2015-03-11 14:41:23 +0800

    Fixed parentActivtyName in ForgetPasswordActivity

--- src/com/droidcare/boundary/HomeActivity.java
+++ src/com/droidcare/boundary/HomeActivity.java
@@ -14,6 +14,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +35,8 @@ public class HomeActivity extends FragmentActivity implements ActionBar.TabListe
 		setContentView(R.layout.activity_home);
 
         Global.initAppointmentManager();
+
+        Log.d("DEBUGGING", "HomeActivity onCreate");
         Global.getAppointmentManager().retrieveAppointmentList(new AppointmentManager.OnFinishListener() {
             @Override
             public void onFinish(String responseText) {
